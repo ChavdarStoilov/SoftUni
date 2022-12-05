0cea490d2592dbde0501e27ea8d8ce17740367dd
Chavdar Stoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2022-12-05 15:42:27 +0200

    Fixed another issue under code, was forgotten default years

--- python_advanced/python_oop/oop_exams/movie_app_python_oop_retake_exam_18_april_2022/project/movie_specification/fantasy.py
+++ python_advanced/python_oop/oop_exams/movie_app_python_oop_retake_exam_18_april_2022/project/movie_specification/fantasy.py
@@ -3,7 +3,7 @@ from project.movie_specification.movie import Movie
 
 class Fantasy(Movie):
 
-    def __init__(self, title: str, year: int, owner: object, age_restriction: int):
+    def __init__(self, title: str, year: int, owner: object, age_restriction = 6):
         super().__init__(title, year, owner, age_restriction)
 
     @property
--- python_advanced/python_oop/oop_exams/movie_app_python_oop_retake_exam_18_april_2022/project/movie_specification/thriller.py
+++ python_advanced/python_oop/oop_exams/movie_app_python_oop_retake_exam_18_april_2022/project/movie_specification/thriller.py
@@ -3,7 +3,7 @@ from project.movie_specification.movie import Movie
 
 class Thriller(Movie):
 
-    def __init__(self, title: str, year: int, owner: object, age_restriction: int):
+    def __init__(self, title: str, year: int, owner: object, age_restriction = 16):
         super().__init__(title, year, owner, age_restriction)
 
 
