b706ba5b65ecedf0d6d41913b6c1262215e92bdd
ChavdarStoilov <80538498+ChavdarStoilov@users.noreply.github.com>
2023-06-30 16:30:14 +0300

    Created register and login function

--- Python Web (Django)/Trainings/Trining_CBV_user_login_register_and_permissions/application/web/urls.py
+++ Python Web (Django)/Trainings/Trining_CBV_user_login_register_and_permissions/application/web/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import IndexView, LoginView, RegisterView
+from .views import IndexView, login_view, RegisterView
 
 urlpatterns = [
     path("", IndexView.as_view(), name='home page'),
-    # path("login/", LoginView.as_view(), name='login page'),
-    # path('register/', RegisterView.as_view(), name='register page'),
+    path("login/", login_view, name='login page'),
+    path('register/', RegisterView.as_view(), name='register page'),
 ]
